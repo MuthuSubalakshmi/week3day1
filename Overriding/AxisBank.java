88136984cdf3ac5f67b099f515d43a22453f66ff
MuthuSubalakshmi <100410577+MuthuSubalakshmi@users.noreply.github.com>
2022-03-05 17:39:29 +0530

    Add files via upload

---
+++ Overriding/AxisBank.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class AxisBank extends BankInfo
+{
+	public void deposit()
+	{
+		System.out.println("Amount deposited");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AxisBank info=new AxisBank();
+info.deposit();
+	}
+
+}
---
+++ Overriding/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving()
+	{
+		System.out.println("Savings/Current");
+	}
+	public void fixed()
+	{
+		System.out.println("fixed/recurring deposit");
+	}
+public void deposit()
+{
+	System.out.println("10000");
+}
+}
